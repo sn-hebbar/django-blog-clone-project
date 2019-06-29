30266a12b86382fec378890d03c8cac89c2ac10d
sn-hebbar <51874404+sn-hebbar@users.noreply.github.com>
2019-06-29 13:12:07 +0530

    Update urls.py
    / after login and logout

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,6 +20,6 @@ from django.contrib.auth import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
-    path('account/logout',views.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
-    path('account/login',views.LoginView.as_view(template_name='registration/login.html'),name='login')
+    path('account/logout/',views.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
+    path('account/login/',views.LoginView.as_view(template_name='registration/login.html'),name='login')
     ]
