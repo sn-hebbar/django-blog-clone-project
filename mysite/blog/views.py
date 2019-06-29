c50e02e7e62a7fb6e6ce341924165e324c5fdca8
sn-hebbar <51874404+sn-hebbar@users.noreply.github.com>
2019-06-29 13:04:37 +0530

    Update views.py
    added login required location

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -52,7 +52,7 @@ class DraftListView(LoginRequiredMixin,ListView):
 
 ################################
 ################################
-@login_required
+@login_required(login_url='/account/login/')
 def add_comment_to_post(request,pk):
     post=get_object_or_404(Post,pk=pk)
     if request.method=="POST":
